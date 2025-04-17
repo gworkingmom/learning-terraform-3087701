bbb6c3bc0f393d303201cf0e679c3484c0225c02
gworkingmom <129624654+gworkingmom@users.noreply.github.com>
2025-04-17 15:17:22 -0400

    misspelling

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "blog" {
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_voc.public_subnets[0]
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
